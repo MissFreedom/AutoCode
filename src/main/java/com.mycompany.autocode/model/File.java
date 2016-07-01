559399570772d069a389b834aabc8e96fe58530d
MissFreedom <985364654@qq.com>
2016-07-01 16:47:20 +0800

    "新建"

--- src/main/java/com.mycompany.autocode/model/File.java
+++ src/main/java/com.mycompany.autocode/model/File.java
@@ -5,5 +5,42 @@ package com.mycompany.autocode.model;
  */
 public class File {
 
-    private String
+    /**
+     * web.xml内容
+     */
+    private String web;
+
+    /**
+     * pom.xml内容
+     */
+    private String pom;
+
+    /**
+     * web首页内容
+     */
+    private String index;
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getPom() {
+        return pom;
+    }
+
+    public void setPom(String pom) {
+        this.pom = pom;
+    }
 }
